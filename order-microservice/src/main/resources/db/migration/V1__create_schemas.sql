5fc2c8e9241f7c415cfdece1135cb0f010a01c3a
Juan Carlos A <abjuancarlos.12@gmail.com>
2026-01-17 00:56:44 -0500

    feat: add first order's table

---
+++ order-microservice/src/main/resources/db/migration/V1__create_schemas.sql
@@ -0,0 +1,7 @@
+-- Schemas
+CREATE SCHEMA sales;
+CREATE SCHEMA pricing;
+CREATE SCHEMA fulfillment;
+CREATE SCHEMA inventory_reservation;
+CREATE SCHEMA payment;
+CREATE SCHEMA projections; -- Para las read models (CQRS)
---
+++ order-microservice/src/main/resources/db/migration/V2__order_table.sql
@@ -0,0 +1,69 @@
+-------------------- order enums ---------------------------------
+CREATE TYPE ORDER_STATUS_ENUM AS ENUM(
+	'Pending',
+	'Confirmed',
+	'Processing',
+	'Cancelled',
+	'Completed'
+);
+
+CREATE TYPE PAYMENT_STATUS_ENUM AS ENUM('Pending', 'Paid', 'Failed');
+
+CREATE TYPE CURRENCY_ENUM AS ENUM('USD', 'EUR', 'GBP', 'MXN');
+
+-------------------- order ---------------------------------
+CREATE TABLE SALES.ORDERS (
+                              ORDER_ID BIGSERIAL PRIMARY KEY,
+                              ORDER_NUMBER VARCHAR(50) NOT NULL UNIQUE,
+                              CUSTOMER_ID BIGINT NOT NULL,
+                              ORDER_DATE TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              ORDER_STATUS ORDER_STATUS_ENUM NOT NULL, --- enum
+                              SUBTOTAL_AMOUNT NUMERIC(18, 2) NOT NULL,
+                              DISCOUNT_AMOUNT NUMERIC(18, 2) NOT NULL DEFAULT 0,
+                              TAX_AMOUNT NUMERIC(18, 2) NOT NULL DEFAULT 0,
+                              SHIPPING_AMOUNT NUMERIC(18, 2) NOT NULL DEFAULT 0,
+                              TOTAL_AMOUNT NUMERIC(18, 2) NOT NULL,
+                              CURRENCY CURRENCY_ENUM NOT NULL DEFAULT 'USD', --- enum
+                              PAYMENT_METHOD VARCHAR(50),
+                              PAYMENT_STATUS PAYMENT_STATUS_ENUM, --- enum
+                              SHIPPING_ADDRESS_ID UUID,
+                              BILLING_ADDRESS_ID UUID,
+                              NOTES TEXT,
+                              SOURCE VARCHAR(50),
+                              IP_ADDRESS INET,
+                              CREATED_AT TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              UPDATED_AT TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              CREATED_BY VARCHAR(100),
+                              UPDATED_BY VARCHAR(100),
+    -- Control de concurrencia optimista
+                              VERSION INTEGER NOT NULL DEFAULT 1,
+                              IS_DELETED BOOLEAN NOT NULL DEFAULT FALSE,
+                              DELETED_AT TIMESTAMPTZ,
+                              CONSTRAINT CK_ORDERS_AMOUNTS_POSITIVE CHECK (
+                                  SUBTOTAL_AMOUNT >= 0
+                                      AND DISCOUNT_AMOUNT >= 0
+                                      AND TAX_AMOUNT >= 0
+                                      AND SHIPPING_AMOUNT >= 0
+                                      AND TOTAL_AMOUNT >= 0
+                                  )
+    -- we are using enums now
+    --                                       CONSTRAINT ck_orders_status
+    --                                       CHECK (order_status IN ('Pending', 'Confirmed', 'Processing', 'Cancelled', 'Completed')),
+    --
+    --                               CONSTRAINT ck_orders_payment_status
+    --                                   CHECK (payment_status IS NULL OR payment_status IN ('Pending', 'Paid', 'Failed')),
+    --
+    --                               CONSTRAINT ck_orders_currency
+    --                                   CHECK (currency IN ('USD', 'EUR', 'GBP', 'MXN')),
+);
+
+-------------------- order index ---------------------------------
+CREATE INDEX IX_ORDERS_CUSTOMER ON SALES.ORDERS (CUSTOMER_ID);
+
+CREATE INDEX IX_ORDERS_STATUS ON SALES.ORDERS (ORDER_STATUS);
+
+CREATE INDEX IX_ORDERS_DATE ON SALES.ORDERS (ORDER_DATE DESC);
+
+CREATE INDEX IX_ORDERS_ACTIVE ON SALES.ORDERS (ORDER_DATE)
+    WHERE
+	IS_DELETED = FALSE;
