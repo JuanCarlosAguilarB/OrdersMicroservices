fe184bbd3c39fea1ca11aa1513f2c90a9d058725
Juan Aguilar <abjuancarlos.12@gmail.com>
2026-02-15 18:51:50 -0500

    fix: controller's product was fixed. It had the wrong path mapping

--- order-microservice/src/main/java/com/example/demo/catalogue/api/ProductController.java
+++ order-microservice/src/main/java/com/example/demo/catalogue/api/ProductController.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
-@RestController("/api/v1/products")
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService service;
